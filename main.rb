502e8db61a51cfe91b35e5f3ffef37e95ed02762
Benjamin Loustau <benloustau@gmail.com>
2014-10-02 15:51:36 -0400

    changes to main

--- main.rb
+++ main.rb
@@ -17,3 +17,25 @@ end
 get '/profile' do
 	erb :profile
 end
+
+get '/contact_us' do
+	erb :contact_us
+end
+
+post '/sign_in' do
+	puts "params are: #{params.inspect}"
+	@user = User.where(email: params[:email]).first
+ 	if @user && @user.password  == params[:password] 
+		session[:user_id] = @user.id
+		redirect'/home'
+  	else
+  		flash[:alert] = "Sorry, that user doesn't exist. Feel free to sign up."
+    	redirect '/index'
+	end
+end	
+
+post '/sign_up' do
+ User.create(params[:user])
+ redirect '/home'
+end	
+	
