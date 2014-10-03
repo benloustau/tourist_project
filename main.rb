7e50f699bb82afe930233ada9887670e37a747ae
Keika Jones <keika.jones@gmail.com>
2014-10-03 10:29:38 -0400

    posts form on profile page

--- main.rb
+++ main.rb
@@ -8,14 +8,6 @@ require 'mandrill'
 
 set :database, "sqlite3:example.sqlite3"
 
-def current_user
-	if session[:user_id]
-		@current_user = User.find(session[:user_id])
-	else
-		nil
-	end
-end
-
 get '/' do
 	erb :index
 end
@@ -43,22 +35,36 @@ post '/sign_in' do
 		session[:user_id] = @user.id
 		redirect'/home'
   	else
-  		flash[:notice] = "Login failed please try again or sign up"
+  		# flash[:notice] = "Login failed please try again or sign up"
     	redirect '/index'
 	end
 end	
 
 post '/sign_up' do
 	User.create(params[:user])
- 	flash[:notice] = "Your account has been created. Please login or sign-up"
- 	redirect '/index'
+ 	# flash[:notice] = "Your account has been created. Please login or sign-up"
+ 	redirect '/edit_profile'
 end	
 
-post 'edit_profile' do
+post '/edit_profile' do
 	puts "params are: #{params.inspect}"
 	Profile.create(params[:profile])
 	flash[:notice] = "Thank you for creating your profile."
 	redirect '/profile'
+end
+
+def current_user
+	if session[:user_id]
+		@current_user = User.find(session[:user_id])
+	else
+		nil
+	end
+end
+
+post '/profile' do
+	puts "params are: #{params.inspect}"
+	Post.create(params[:post])
+	# redirect '/profile'
 end
 
 post '/send_email' do
